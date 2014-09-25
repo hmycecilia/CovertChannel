8c815c8042fa36f3a9ec74f6a9fa449889fe2c7a
majisto <majisto@gmail.com>
2014-09-25 16:43:17 -0500

    Commiting Eclipse Project files.

--- CovertChannel/src/CovertChannel.java
+++ CovertChannel/src/CovertChannel.java
@@ -1,10 +1,8 @@
 
-public class CovertChannel {
-
+public class CovertChannel 
+{
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
 		System.out.println("IT'S OVER 9,000?!?!!");
 	}
-
 }
