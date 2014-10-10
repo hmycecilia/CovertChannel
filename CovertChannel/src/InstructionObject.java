371df88b4e21976b64d296de1c3a99a75a8c0cac
majisto <majisto@gmail.com>
2014-10-09 21:01:08 -0500

    Trying to fix git... :(

--- CovertChannel/src/InstructionObject.java
+++ CovertChannel/src/InstructionObject.java
@@ -1,9 +1,12 @@
 
 public class InstructionObject 
 {
-	private static final String r = "read";
-	private static final String w = "write";
+//	private static final String r = "read";
+//	private static final String w = "write";
 	private static final String b = "BAD";
+//	public static final String c = "create";
+//	public static final String d = "destroy";
+//	public static final String run = "run";
 	
 	public String command;
 	public String subject;
@@ -15,37 +18,31 @@ public class InstructionObject
 		String delim = "[ ]+";
 		String[] tokens = a.split(delim);
 
-		//check if is a valid instruction, if is then assign to variables
-		if (tokens.length == 3 || tokens.length == 4)
+		if(tokens.length == 2)
 		{
-            command = tokens[0].toLowerCase();
-            
-            if (!command.equals(r) && !command.equals(w))
-            {
-            	command = b;
-            }
-            
-            subject = tokens[1].toLowerCase();
-            object = tokens[2].toLowerCase(); 
-            if(command.equals(w) && tokens.length == 4)
-            {
-            	try {
+			command = tokens[0].toLowerCase();
+			subject = tokens[1].toLowerCase(); 
+		}
+		else if(tokens.length == 3)
+		{
+			command = tokens[0].toLowerCase();
+			subject = tokens[1].toLowerCase();
+			object = tokens[2].toLowerCase(); 
+		}
+		else
+		{
+			command = tokens[0].toLowerCase();
+			subject = tokens[1].toLowerCase();
+			object = tokens[2].toLowerCase(); 
+			try {
             	value = Integer.parseInt(tokens[3]);}
             	catch (java.lang.NumberFormatException e)
             	{
             		command = b;
             		return;
             	}
-            }else if(!command.equals(r))
-            {
-            	command = b;
-            }
-            
-		} else 
-		{
-			command = b;
 		}
 		
-		
+		System.out.println("Instruction: " + command + " " + subject + " " + object + " " + value);
 	}
 }
